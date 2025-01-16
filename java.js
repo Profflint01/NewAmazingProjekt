2fe58d799b3f54c13858dd9608662c01738f7b58
Ismet KONAN (EXT) <ismet.konan.ext@valeo.com>
2025-01-16 13:43:49 +0100

    new

--- java.js
+++ java.js
@@ -1,13 +1,18 @@
 let loadingBar = document.getElementById('loading-bar');
+let loadingContainer = document.getElementById('loading-container');
+let userImage = document.getElementById('userImage');
 let width = 0;
 
 function increaseLoadingBar() {
     if (width >= 100) {
         clearInterval(loadingInterval);
+        loadingContainer.style.display = 'none';
+        document.body.style.backgroundColor = 'white';
+        userImage.style.display = 'block';  // Correctly show the image
     } else {
-        width += 1;
+        width += 2;
         loadingBar.style.width = width + '%';
     }
 }
 
-let loadingInterval = setInterval(increaseLoadingBar, 1);
+let loadingInterval = setInterval(increaseLoadingBar, 100);
